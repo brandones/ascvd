672ad18f9ca024f617a7dfaed60a2b4b5b1e3766
Brandon Istenes <bistenes@gmail.com>
2018-10-16 21:56:58 -0500

    Get things working and kind of documented

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python
 
+import setuptools
 from distutils.core import setup
+import os
+
+# Get the long description from the README file
+with open(os.path.join(".", "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
-    name="ASCVD",
-    version="0.1",
+    name="ascvd",
+    version="0.5",
     description="Module for calculating ASCVD 10-year estimate",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Brandon Istenes",
     author_email="brandonesbox@gmail.com",
-    url="",
+    url="https://github.com/brandones/ascvd",
     packages=["ascvd"],
+    project_urls={"Source": "https://github.com/brandones/ascvd"},
 )
